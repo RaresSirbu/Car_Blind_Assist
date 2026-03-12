5e8eeb6e7ba788b5328b64a2022fb88c99ce7272
Sirbu Rares <63116357+RaresSirbu@users.noreply.github.com>
2026-03-12 19:48:41 +0200

    Active_Blind_Assist_Proiect.cpp

--- Active_Blind_Assist_Proiect.cpp
+++ Active_Blind_Assist_Proiect.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-
 #define TRIG_STANG 7
 #define ECHO_STANG 6
 #define TRIG_DREPT 5
@@ -9,11 +8,9 @@
 #define LED_STANG 8
 #define LED_DREPT 9
 #define BUZZER 10
-
 const float DISTANTA_PRAG = 50.0;
 const unsigned long INTERVAL_SENZOR = 100;
 const int MARIME_FILTRU = 5;
-
 float distanteStang[MARIME_FILTRU];
 float distanteDrept[MARIME_FILTRU];
 int indexStang = 0;
@@ -24,16 +21,13 @@ float distantaStangFiltrata = 999.0;
 float distantaDreptFiltrata = 999.0;
 unsigned long ultimaSchimbareBuzzer = 0;
 const unsigned long INTERVAL_BUZZER = 300;
-
 enum StareLaterala {
   SIGUR,
   OBIECT_DETECTAT,
   AVERTISMENT_ACTIV
 };
-
 StareLaterala stareStang = SIGUR;
 StareLaterala stareDrept = SIGUR;
-
 float citesteUltrasonic(int trigPin, int echoPin) {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -44,13 +38,11 @@ float citesteUltrasonic(int trigPin, int echoPin) {
   if (durata == 0) return 999.0;
   return durata * 0.034 / 2;
 }
-
 float calculeazaMedie(float* arr) {
   float suma = 0;
   for (int i = 0; i < MARIME_FILTRU; i++) suma += arr[i];
   return suma / MARIME_FILTRU;
 }
-
 void actualizeazaStare(StareLaterala &stare, bool obiectDetectat, bool butonApasat) {
   switch (stare) {
     case SIGUR:
@@ -66,7 +58,6 @@ void actualizeazaStare(StareLaterala &stare, bool obiectDetectat, bool butonApas
       break;
   }
 }
-
 void setup() {
   pinMode(TRIG_STANG, OUTPUT);
   pinMode(ECHO_STANG, INPUT);
@@ -77,41 +68,33 @@ void setup() {
   pinMode(LED_STANG, OUTPUT);
   pinMode(LED_DREPT, OUTPUT);
   pinMode(BUZZER, OUTPUT);
-  
   for (int i = 0; i < MARIME_FILTRU; i++) {
     distanteStang[i] = 999.0;
     distanteDrept[i] = 999.0;
   }
 }
-
 void loop() {
   unsigned long acum = millis();
-  
   if (acum - ultimaCitireStang >= INTERVAL_SENZOR) {
     ultimaCitireStang = acum;
     distanteStang[indexStang] = citesteUltrasonic(TRIG_STANG, ECHO_STANG);
     distantaStangFiltrata = calculeazaMedie(distanteStang); 
     indexStang = (indexStang + 1) % MARIME_FILTRU;
-  }
-  
+  }  
   if (acum - ultimaCitireDrept >= INTERVAL_SENZOR) {
     ultimaCitireDrept = acum;
     distanteDrept[indexDrept] = citesteUltrasonic(TRIG_DREPT, ECHO_DREPT);
     distantaDreptFiltrata = calculeazaMedie(distanteDrept);
     indexDrept = (indexDrept + 1) % MARIME_FILTRU;
-  }
-  
+  }  
   actualizeazaStare(stareStang,
     distantaStangFiltrata < DISTANTA_PRAG,
     digitalRead(BUTON_STANG) == LOW);
-
   actualizeazaStare(stareDrept,
     distantaDreptFiltrata < DISTANTA_PRAG,
-    digitalRead(BUTON_DREPT) == LOW);
-    
+    digitalRead(BUTON_DREPT) == LOW);  
   digitalWrite(LED_STANG, (stareStang != SIGUR) ? HIGH : LOW);
-  digitalWrite(LED_DREPT, (stareDrept != SIGUR) ? HIGH : LOW);
-  
+  digitalWrite(LED_DREPT, (stareDrept != SIGUR) ? HIGH : LOW);  
   if (stareStang == AVERTISMENT_ACTIV || stareDrept == AVERTISMENT_ACTIV) {
     if (acum - ultimaSchimbareBuzzer >= INTERVAL_BUZZER) {
       ultimaSchimbareBuzzer = acum;
@@ -121,3 +104,4 @@ void loop() {
     digitalWrite(BUZZER, LOW);
   }
 }
+
